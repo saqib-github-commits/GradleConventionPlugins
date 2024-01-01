bae0b5c0760a729fa468f370ccb383639b3acc9b
saqib <saqib@saqib.com>
2024-01-01 23:50:48 +0100

    Gradle Convention plugins for android library and application

---
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -0,0 +1,34 @@
+import com.android.build.api.dsl.ApplicationExtension
+import com.example.gradleplugins.configureKotlinAndroid
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class AndroidApplicationConventionPlugin: Plugin<Project> {
+    override fun apply(project: Project) {
+        with(project) {
+            with(project.pluginManager) {
+                apply("com.android.application")
+                apply("org.jetbrains.kotlin.android")
+            }
+
+            extensions.configure<ApplicationExtension> {
+                configureKotlinAndroid(this)
+                configureDefaultConfig()
+            }
+        }
+    }
+}
+
+private fun ApplicationExtension.configureDefaultConfig() {
+    defaultConfig {
+        applicationId = "com.sample.gradleplugins"
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+}
